7ba9ae3d0d9b67d4dc432be34e2a79b1f3b8350b
Bruno Postle <bruno@postle.net>
2021-01-13 22:50:25 +0000

    use TopologicCore headers in /usr/include or /usr/local/include if available

--- cpython/topologic/__init__.py
+++ cpython/topologic/__init__.py
@@ -62,12 +62,19 @@ if (os.path.isfile("/usr/local/include/opencascade/TopoDS_Shape.hxx")):
 elif (os.path.isfile("/usr/include/opencascade/TopoDS_Shape.hxx")):
     cppyy.add_include_path("/usr/include/opencascade")
 base_dir = os.path.dirname(os.path.realpath(__file__))
-cppyy.add_include_path(base_dir + "/include")
+
+if (os.path.isfile("/usr/local/include/TopologicCore/Topology.h")):
+    topologic_inc = "/usr/local/include/TopologicCore"
+elif (os.path.isfile("/usr/include/TopologicCore/Topology.h")):
+    topologic_inc = "/usr/include/TopologicCore"
+elif (os.path.isfile(base_dir + "/include/Topology.h")):
+    topologic_inc = base_dir + "/include"
+
+cppyy.add_include_path(topologic_inc)
+
 for header in headers:
-    cppyy.include( base_dir + "/include/" + header )
+    cppyy.include(topologic_inc + "/" + header )
 
-
-#cppyy.load_library("libTopologicCore.so")
 cppyy.load_library("TopologicCore")
 
 from cppyy.gbl import TopologicCore
